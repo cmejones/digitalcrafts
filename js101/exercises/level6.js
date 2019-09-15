db3d7f619bb9b84ab450e48db0d729d9409d12a1
cmejones <1016cme@gmail.com>
2019-09-15 16:24:50 -0400

    adding levels 4, 5, and some of 6 eexercises

--- js101/exercises/level6.js
+++ js101/exercises/level6.js
@@ -1,6 +1,12 @@
 function lvl6exercise1 (num) {
   // Return 'hello' if num is 1, 'world' if num is 2, otherwise return 'bye'
+ if (num == 1) {
+  return "hello"; 
+} else if (num == 2) {
+  return "world"; 
+} else return "bye";
 }
+console.log(lvl6exercise1(5));
 
 function lvl6exercise2 () {
   // Push 10 'hello' strings into the array using a for loop, then return it
