6dbf429f6690a88e099d924d0fbec6bfaeb6c84b
cmejones <1016cme@gmail.com>
2019-10-19 13:24:54 -0400

    adding social project

---
+++ 10_17_classWork/pokemon.js
@@ -0,0 +1,31 @@
+/**
+ * Using the documentation found here:
+ * 
+ * https://www.w3schools.com/jsref/jsref_foreach.asp
+ * 
+ * Iterate through the arrayin a foreach each loop,
+ * and console log the pokemen name and attack power as such:
+ * "Pokemen Pikachu has an attack power of 500"
+ */
+var pokemon = [
+    {
+        name : "Pickachu",
+        attackPower: 500
+    },
+    {
+        name : "Charzard",
+        attackPower: 1500
+    },
+    {
+        name : "Kakkarot",
+        attackPower: 9000
+    }    
+];
+
+pokemon.forEach(pokemonFunction);
+
+function pokemonFunction(item, arr) {
+    console.log("Pokemon "  + item.name + " has an attack power of", item.attackPower);
+}
+
+
